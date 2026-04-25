94ddf1cc9f04336f643da4138e26461aee632ad8
Debashis Goswami <145210990+Bittu3487@users.noreply.github.com>
2026-04-25 07:59:21 +0530

    feat: add task model

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
-
+app.use('/api/tasks', require('./routes/tasks'))
 // MongoDB Connect
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected! ✅'))
