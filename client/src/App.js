cfa44e3e7218d65514666d2cc1a396d124a9fd00
Debashis Goswami <145210990+Bittu3487@users.noreply.github.com>
2026-04-25 08:57:27 +0530

    fix:remove unused

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,6 @@ import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 
 function App() {
-  const token = localStorage.getItem('token')
-
   return (
     <Router>
       <Routes>
